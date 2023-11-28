294ce6bd5f805c55ae2b4123b8c38205d522d3c6
Baber Ibrar <babar.spyresync@gmail.com>
2023-11-28 12:33:28 +0500

    Add Python code to print

---
+++ Day 4/Our_First_Python_Program.py
@@ -0,0 +1,8 @@
+# This line prints the string "Hello World!" to the console.
+print("Hello World!")
+
+# This line prints the integer 5 to the console.
+print(5)
+
+# This line prints the string 'Bye' to the console.
+print('Bye')
---
+++ Day 4/README.md
@@ -0,0 +1,5 @@
+# Summary:
+
+# * The first line prints the classic "Hello World!" message.
+# * The second line prints the number 5.
+# * The third line prints the word 'Bye'.
